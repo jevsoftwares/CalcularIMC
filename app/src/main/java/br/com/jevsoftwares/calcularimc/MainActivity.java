84c330584af26be4f168f7b53747684c0e243db2
J & V Softwares <jevsoftwares@gmail.com>
2021-09-22 01:11:42 -0400

    Projeto Finalizado.

--- app/src/main/java/br/com/jevsoftwares/calcularimc/MainActivity.java
+++ app/src/main/java/br/com/jevsoftwares/calcularimc/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.jevsoftwares.calcularimc;
 
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,12 +35,44 @@ public class MainActivity extends AppCompatActivity {
                     psS = ps.getText().toString();
                 double psF = Double.parseDouble(psS);
                 String altS = "0.0";
-                if (!ps.getText().toString().isEmpty())
-                    altS = ps.getText().toString();
+                if (!alt.getText().toString().isEmpty())
+                    altS = alt.getText().toString();
                 double altF = Double.parseDouble(altS);
                 double imc = psF / Math.pow(altF,2);
 
-                result.setText(String.valueOf(imc));
+                if (imc < 18.5){
+                    result.setText(String.valueOf(imc)+"\nAbaixo do peso");
+                    result.setTextColor(Color.RED);
+                }
+                else if (imc >= 18.5 && imc <= 24.9){
+                    result.setText(String.valueOf(imc)+"\nNormal");
+                    result.setTextColor(Color.GREEN);
+                }
+                else if (imc > 24.9 && imc <= 29.9){
+                    result.setText(String.valueOf(imc)+"\nSobrepeso");
+                    result.setTextColor(Color.RED);
+
+                }
+                else if (imc > 29.9 && imc <= 34.9){
+                    result.setText(String.valueOf(imc)+"\nObesidade nível I");
+                    result.setTextColor(Color.RED);
+
+                }
+                else if (imc > 34.9 && imc <= 39.9){
+                    result.setText(String.valueOf(imc)+"\nObesidade nível II");
+                    result.setTextColor(Color.RED);
+
+                }
+                else if (imc > 39.9 && imc <= 49.9){
+                    result.setText(String.valueOf(imc)+"\nObesidade nível III");
+                    result.setTextColor(Color.RED);
+
+                }
+                else if (imc > 49.9){
+                    result.setText(String.valueOf(imc)+"\nObesidade nível IV");
+                    result.setTextColor(Color.RED);
+
+                }
             }
         });
     }
